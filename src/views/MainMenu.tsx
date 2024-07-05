c190b058605d6679b98eba7cc0a7709144e52049
Andreas Lauvhjell <andreas.lauvhjell@no.experis.com>
2024-07-05 14:59:27 +0200

    Weiter mit den Menu

--- src/views/MainMenu.tsx
+++ src/views/MainMenu.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { useBackground } from '../services/contexts/BackgroundContext';
 import MainMenuBackground from '../assets/images/backgroundimages/main/MainMenu.png';
-import MainMenuList from '../components/buttons/buttonlists/MainMenuList';
+import MainMenuList from '../components/buttons/buttonlists/mainmenu/MainMenuList';
 import MainTheme from '../assets/audio/music/00maintheme.mp3';
 import Btn from '../components/buttons/Button';
 
 import Modal from '../components/Modal';
 import '../assets/styles/Modal.css'
+import '../assets/styles/MainMenu.css'
 
 
 function MainMenu() {
@@ -17,7 +18,12 @@ function MainMenu() {
   }, [setBackground]);
 
   return (
-    <MainMenuList />
+    <>
+      <div className="main-menu-placement">
+        <p>here</p>
+        {/* <MainMenuList /> */}
+      </div>
+    </>
   );
 }
 
